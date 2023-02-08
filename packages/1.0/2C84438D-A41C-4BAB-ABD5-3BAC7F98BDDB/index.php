58b555131f0dd677a32db3e5dda7422231547d6b
Nigel Grange <ngrange@triller.co>
2023-02-08 21:23:18 +0000

    Name test

--- packages/1.0/2C84438D-A41C-4BAB-ABD5-3BAC7F98BDDB/index.php
+++ packages/1.0/2C84438D-A41C-4BAB-ABD5-3BAC7F98BDDB/index.php
@@ -3,6 +3,9 @@
 function main(array $args) : array
 {
     $object = array('output' => 'test');
+    if (isset($args['name'])) {
+        $object['name'] = $args['name'];
+    }
  
     return [
         'body' => json_encode($object),
