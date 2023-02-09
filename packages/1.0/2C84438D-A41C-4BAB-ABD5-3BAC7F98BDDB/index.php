d5026bf822b34ce04dcddc8ddc14c1d7a44094c2
Nigel Grange <ngrange@triller.co>
2023-02-09 10:34:06 +0000

    Add request obj

--- packages/1.0/2C84438D-A41C-4BAB-ABD5-3BAC7F98BDDB/index.php
+++ packages/1.0/2C84438D-A41C-4BAB-ABD5-3BAC7F98BDDB/index.php
@@ -2,19 +2,48 @@
  
 function main(array $args) : array
 {
-    $json = file_get_contents('php://input');
-    // Converts it into a PHP object
-    $data = json_decode($json);
+    if (
+        !isset($array['sdk']) ||
+        !isset($array['installID']) ||
+        !isset($array['bundleID']) ||
+        !isset($array['appVersion']) ||
+        !isset($array['buildVersion']) ||
+        !isset($array['osVersion']) ||
+        !isset($array['device']) ||
+        !isset($array['model']) ||
+        !isset($array['locale'])
+    ) {
+        return [
+            'statusCode' => 403
+        ];    
+    }
 
-    $object = array('output' => 'test');
-    if (isset($args['name'])) {
-        $object['name'] = $args['name'];
-    }
-    if (isset($data['form'])) {
-        $object['form'] = $data['form'];
-    }
-    if (isset($args['post'])) {
-        $object['post'] = $args['post'];
+    $sdkVersion = $array['sdk'];
+    $installID = $array['installID'];
+    $bundleID = $array['bundleID'];
+    $appVersion = $array['appVersion'];
+    $buildVersion = $array['buildVersion'];
+    $osVersion = $array['osVersion'];
+    $device = $array['device'];
+    $model = $array['model'];
+    $locale = $array['locale'];
+
+    
+    $object = array(
+        'sdkVersion' => $sdkVersion,
+        'installID' => $installID,
+        'bundleID' => $bundleID,
+        'appVersion' => $appVersion,
+        'buildVersion' => $buildVersion,
+        'osVersion' => $osVersion,
+        'device' => $device,
+        'model' => $model,
+        'locale' => $locale
+    );
+
+
+    if (isset($args['userID'])) {
+        $object['userID'] = $args['userID'];
     }
  
     return [
